27e823ec551a268cb54d770b52afd86c7a266d50
Gabriel-Henrique <gh5931808@gmail.com>
2025-06-02 09:07:10 -0300

    api

--- WebApiFnal/Models/Produtos.cs
+++ WebApiFnal/Models/Produtos.cs
@@ -9,6 +9,21 @@
         public int Quantidade { get; set; }
 
         public decimal Valor {  get; set; }
-        public DateTime DataCadastro {  get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        
+
+
+        // TIPOS DE DADOS
+
+        string nome = "etec"; //Tipo de Referencia
+        char letra = 'A'; 
+
+        int valor = 10; // numeros Inteiros
+        long valor1 = 10L; // numeros Inteiros
+        byte valorB = 128; // numeros Inteiros
+
+        float valorf = 10.5F; // numeros reais
+        double valor2 = 10.5; // numeros reais
     }
 }
