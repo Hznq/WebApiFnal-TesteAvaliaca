3321df51e834f8d82c0ef13d00b9c5ffa9fb8e72
Gabriel-Henrique <gh5931808@gmail.com>
2025-06-09 09:08:55 -0300

    09062025

--- WebApiFnal/Program.cs
+++ WebApiFnal/Program.cs
@@ -1,13 +1,32 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using WebApiFnal.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<WebApiFnalContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("WebApiFnalContext") ?? throw new InvalidOperationException("Connection string 'WebApiFnalContext' not found.")));
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//politicas de Cors origin
+builder.Services.AddCors(etecPoliticas =>
+{
+    etecPoliticas.AddPolicy("EtecAll",
+        builder =>
+        {
+            builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+
+});
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,11 +34,15 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("EtecAll");
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    app.UseSwagger(); 
     app.UseSwaggerUI();
 }
+
+
 
 app.UseHttpsRedirection();
 
